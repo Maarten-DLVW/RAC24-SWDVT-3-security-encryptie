f0f08e08f2c9571e824d6b441d0db11c8d3618a8
Maarten <m.deleeuwvanweenen2003@gmail.com>
2024-10-13 16:08:26 +0200

    ADD divided into seperate functions

--- cryptography_encryption.py
+++ cryptography_encryption.py
@@ -2,15 +2,21 @@ from cryptography.fernet import Fernet
 
 key = Fernet.generate_key()
 f = Fernet(key)
-print(key)
 
 # encrypting
-plain_text = input("enter message to encrypt: ")
-plain_text_bytes = str.encode(plain_text)
-cipher_text = f.encrypt(plain_text_bytes)
-print(cipher_text)
+def encrypt_text():
+    plain_text = input("enter message to encrypt: ")
+    plain_text_bytes = str.encode(plain_text)
+    cipher_text = f.encrypt(plain_text_bytes)
+    print(f"encrypted message: {cipher_text}")
 
 #decrypting
-decrypted_text_bytes = f.decrypt(cipher_text)
-decrypted_text = decrypted_text_bytes.decode()
-print(decrypted_text)
+def decrypt_text():
+    encrypted_text_input = input("enter encrypted message to decrypt: ")
+    decrypted_text_bytes = f.decrypt(encrypted_text_input)
+    decrypted_text = decrypted_text_bytes.decode()
+    print(f"decrypted message: {decrypted_text}")
+    
+if __name__ == '__main__':
+    encrypt_text()
+    decrypt_text()
