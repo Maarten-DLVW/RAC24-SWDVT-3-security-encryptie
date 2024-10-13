140cb20ccdef1b21bab43d48b9320644d624ee70
Maarten <m.deleeuwvanweenen2003@gmail.com>
2024-10-13 16:18:52 +0200

    ADD divided string encryption into seperate functions

--- string_encryption.py
+++ string_encryption.py
@@ -10,23 +10,29 @@ key = chars.copy()
 random.shuffle(key)
 
 # encrypting
-plain_text = input("enter message to encrypt: ")
-cipher_text = ""
+def encrypt_message():
+    plain_text = input("enter message to encrypt: ")
+    cipher_text = ""
 
-for letter in plain_text:
-    index = chars.index(letter)
-    cipher_text += key[index]
+    for letter in plain_text:
+        index = chars.index(letter)
+        cipher_text += key[index]
 
-print(f"original message: {plain_text}")
-print(f"encrypted message: {cipher_text}")
+    print(f"original message: {plain_text}")
+    print(f"encrypted message: {cipher_text}")
 
 # decrypting
-cipher_text = input("enter message to decrypt: ")
-plain_text = ""
+def decrypt_message():
+    cipher_text = input("enter message to decrypt: ")
+    plain_text = ""
 
-for letter in cipher_text:
-    index = key.index(letter)
-    plain_text += chars[index]
+    for letter in cipher_text:
+        index = key.index(letter)
+        plain_text += chars[index]
 
-print(f"encrypted message: {cipher_text}")
-print(f"original message: {plain_text}")
+    print(f"encrypted message: {cipher_text}")
+    print(f"original message: {plain_text}")
+
+if __name__ == '__main__':
+    encrypt_message()
+    decrypt_message()
